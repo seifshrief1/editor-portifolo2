acdd8c4b9e345449afd73775b351d4ef178361c5
mostafaomar12 <mostafaomar1215@gmail.com>
2024-10-27 22:17:40 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" exact element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/services' element={<Services />} />
         <Route path='/contact' element={<Contact />} />
